30335f88940f90a1dc7a9e4cf5add3ad6f43705f
Nataly Verdugo <natalyverdugo@sarcan.cl>
2019-04-16 11:45:25 -0400

    usuario y token listo

--- app.js
+++ app.js
@@ -3,29 +3,35 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-
 // Inicializar variables
 var app = express();
 
+
 // Body Parser
-// parse application/x-www-form-urlencoded  parse application/json
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 // Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, res) => {
+
     if (err) throw err;
 
-    console.log('Base de datos 3000: \x1b[32m%s\x1b[0m', 'online');
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
 });
 
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
 
